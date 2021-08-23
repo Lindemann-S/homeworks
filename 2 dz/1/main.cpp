febd2e9ee1a73e8eec275ae0c81633a70246aa9a
Lindemann-S <lisovin1889@gmail.com>
2021-08-23 17:31:21 +0300

    change function names and other...

--- 2 dz/1/main.cpp
+++ 2 dz/1/main.cpp
@@ -3,15 +3,19 @@
 
 using namespace std;
 
-unsigned long long itfib(int num)
+unsigned long long iterativeFibonacci(int num)
 {
     unsigned long long n1 = 0;
     unsigned long long n2 = 1;
     unsigned long long sum = 0;
+    if (num < 0)
+    {
+        cout << "negative value ";
+        return 0;
+    }
     if (num == 0)
     {
-        n2 = 0;
-        return n2;
+        return 0;
     }
     for (int i = 0; i < num - 1; i++)
     {
@@ -22,8 +26,13 @@ unsigned long long itfib(int num)
     return n2;
 }
 
-unsigned long long recfib(int num)
+unsigned long long recursiveFibonacci(int num)
 {
+    if (num < 0)
+    {
+        cout << "negative value ";
+        return 0;
+    }
     if (num == 0)
     {
         return 0;
@@ -32,7 +41,7 @@ unsigned long long recfib(int num)
     {
         return 1;
     }
-    return recfib(num - 1) + recfib(num - 2);
+    return recursiveFibonacci(num - 1) + recursiveFibonacci(num - 2);
 }
 
 int main()
@@ -40,9 +49,7 @@ int main()
     int num = 0;
     cout << "enter the number of the Fibonacci number: ";
     cin >> num;
-    unsigned long long result1 = itfib(num);
-//    unsigned long long result2 = recfib(num);
-//    cout << result1 << ", " << result2 << endl;
-    cout << result1;
+    unsigned long long result = iterativeFibonacci(num);
+    cout << result;
     return 0;
 }
